4261330932256de8aea6364920ffbca0f2a95529
ClimbsRocks <climbsbytes@gmail.com>
2016-02-20 20:18:15 -0800

    more ideas. many food-based. i might be hungry...

--- dateIdeas.js
+++ dateIdeas.js
@@ -145,6 +145,31 @@ spend an hour blindfolded. both of you blindfolded, then one of you blindfolded 
 spend an hour doing something together with ear plugs in, and not talking to each other. laughing is (oddly) discouraged. board games might be a good one. or looking through a book on artwork together. or cooking together. or giving each other massages. 
 make up your own games together!
 
+play music for each other from your music collection. maybe while driving a car. 
+play nothing but music covers for each other for hours at a time. 
+build a pillow fort!
+pick one of each others' favorite dishes, and see if you can create something inspired by that, but different enough to not be compared to it. 
+grab 100 stickers each. hide them throughout each others stuff. whoever finds one last wins. 
+go enjoy some of the enormous previously-industrial spaces in the city. this doubles as hanging out with fun burning man camps :)
+bike or drive to one of the empty bluffs overlooking the ocean. hammock or blanket under the stars while the waves wash in from the West. 
+pizza tasting night- go to a ton of pizza places. buy only one slice at each place (not one slice each- one slice). 
+have a night where you only wear things from the other persons' wardrobe. 
+go whale watching to the Farralon Islands!
+take a walking tour of different neighborhoods. there are apps for this, or do it with a guide. 
+make christmas cookies together in the middle of the year!
+try acro-yoga together. the person with the most bruises wins (they trusted their partner the most!)
+try all the slides around. even the ones that don't exist. 
+run down Lombard street. then run up it! try doing this on something wheeled where the other person has all the control (shopping cart, tandem bike, etc.)
+go through chinatown. 
+the russian spas!
+see how many stickers or notes or googly eyes you can hide in the other persons' stuff by the end of the night. 
+formal picnic in the park. ideally in thrift store clothes. 
+make your own candles!
+try your hand at pottery
+anything at that intercultural transcendental center phillip ventura went to. buddhist chanting, cathartic meditation. it's something like the iscc. 
+ride a train all night. 
+
+
 
 
 
