8381fe1c6bcc63e90ffcf354482aac35c6179da4
ClimbsRocks <climbsbytes@gmail.com>
2016-02-20 17:17:31 -0800

    more date ideas. less creative this round. possibly snarkier.

--- dateIdeas.js
+++ dateIdeas.js
@@ -1,5 +1,6 @@
 // each key (number) represents an approximate energy level.
 // to add in a new date idea, just pick the rough energy level, and add it in following the same format as the other dates!
+// The indentation on this breaks all style guides. I'm ok with it. 
 var dateIdeas = {
   0: [{name: 'Cuddling on the Couch', 
         description: 'Light a candle on the table and dive under the blanket together. Likelihood of falling asleep mid-cuddle is high.'
@@ -9,6 +10,9 @@ var dateIdeas = {
       }],
   2: [{name: 'Hammock',
         description: 'Lay in hammock. Cuddle. Point out constellations inaccurately. Hope that the two Newfoundlands this hammock is strapped to don\'t dive into the water with us.'
+      },
+      {name: 'Eat way too much popcorn',
+        description: 'Enter the movies. Buy tub of endless popcorn. Watch movie. Surreptitiously (you know, with the high-knee walking, hunched back, and cackling laughter) sneack into the next film. Refill popcorn. Repeat. Make sure to go to a new popcorn counter each time. Wear boring clothes. Pass out in a butter-drenched haze of explosions and implausibly perfect relationships. Savor your own imperfect match even more.'
       }],
   3: [{name: 'Bonfire at the beach',
         description: 'Fire. Gentle drumbeat of waves. "The next time the sand shares our weight as the sunset glazes the horizon, I\'d love to wrap your fingers through mine, and see what happens."'
@@ -27,7 +31,9 @@ var dateIdeas = {
         description: 'Ok, admittedly, most people don\'t consider hauling around bags of your own poo a particularly romantic activity. And most of us would never be able to keep up with Alex Honnold, Lynn Hill, and Tommy Caldwell. Happily, due to the magic of Google Maps, you can enjoy all this from the comfort of your own armchair! https://www.google.com/maps/about/behind-the-scenes/streetview/treks/yosemite/'
 
       }],
-  5: [],
+  5: [{name: 'Artisanal Popcorn Showdown',
+        description: 'Get giant box of raw popcorn kernels. No, not the kind that come pre-deliciousized for you by Orville Reddenbacher (sp?), but the kind you have to pop on the stove yourself. Bring flavors that would be at home in a crazy donut shop, a farmers market, or the latest hipster haunt. Compare batches, tournament style. Make up crazy names for each flavor combination you conjure up. Picture world domination and population-wide health improvements through running a chain of artisanal popcorn shops.'
+      }],
   6: [{name: 'Board Games at a Bar',
         description: 'Many bars around have board games! What better way to test out your date\'s capability of world domination? Popular places include: Victory Pointe Cafe (Berkeley), Sycamore, The Wreck Room, Soda Popinski\'s, and anything else on this list: https://www.yelp.com/search?find_desc=board+games+bar&find_loc=San+Francisco%2C+CA'
       }],
