5dddee618731faa760ba48b4dd9559f0d05a01cd
ClimbsRocks <climbsbytes@gmail.com>
2016-02-20 17:02:13 -0800

    adds initial date ideas! these are fun to think up :)

---
+++ dateIdeas.js
@@ -0,0 +1,48 @@
+// each key (number) represents an approximate energy level.
+// to add in a new date idea, just pick the rough energy level, and add it in following the same format as the other dates!
+var dateIdeas = {
+  0: [{name: 'Cuddling on the Couch', 
+        description: 'Light a candle on the table and dive under the blanket together. Likelihood of falling asleep mid-cuddle is high.'
+      }],
+  1: [{name: 'Indoor stargazing',
+        description: 'Lay on couch (or hammock!) indoors. Pull out Google Sky. Tell stories of where you imagine the two of us seeing these stars in person. Avoid booking more than 2 trips tonight.'
+      }],
+  2: [{name: 'Hammock',
+        description: 'Lay in hammock. Cuddle. Point out constellations inaccurately. Hope that the two Newfoundlands this hammock is strapped to don\'t dive into the water with us.'
+      }],
+  3: [{name: 'Bonfire at the beach',
+        description: 'Fire. Gentle drumbeat of waves. "The next time the sand shares our weight as the sunset glazes the horizon, I\'d love to wrap your fingers through mine, and see what happens."'
+      },
+      {name: 'Make up your own constellations',
+        descriptions: 'The ancients aren\'t the only ones capable of seeing fantastic things in the sky. I\'m pretty sure that\'s my younger brother up there wrestling an ice cream cone in a ball pit.'
+      }],
+  4: [{name: 'Guacamole Tour',
+        description: 'Have one person pick up a smorgasboard of their favorite guacamoles. Bonus combinations come from picking up an equally large array of favorite things to dip in guacamole (be careful- carrots are contentious, chocolate-dipped bacon will get you creativity points, and your date\'s toe will quickly establish whether they share your foot fetish or not).'
+      },
+      {name: 'Remote Museum Tour',
+        description: ''
+
+      },
+      {name: 'Climb El Cap',
+        description: 'Ok, admittedly, most people don\'t consider hauling around bags of your own poo a particularly romantic activity. And most of us would never be able to keep up with Alex Honnold, Lynn Hill, and Tommy Caldwell. Happily, due to the magic of Google Maps, you can enjoy all this from the comfort of your own armchair! https://www.google.com/maps/about/behind-the-scenes/streetview/treks/yosemite/'
+
+      }],
+  5: [],
+  6: [{name: 'Board Games at a Bar',
+        description: 'Many bars around have board games! What better way to test out your date\'s capability of world domination? Popular places include: Victory Pointe Cafe (Berkeley), Sycamore, The Wreck Room, Soda Popinski\'s, and anything else on this list: https://www.yelp.com/search?find_desc=board+games+bar&find_loc=San+Francisco%2C+CA'
+      }],
+  7: [{name: 'Escape the Room!', 
+        description: 'Pit your combined wits against an impending zombie horde, or time-travelling machine about to go haywire, in one of the many escape-the-room games around. If you\'re really unlucky, you might just time travel all the zombies to the past, when our ancestors were particularly susceptible because they hadn\'t seen all the movies yet and prepared for the zombie apocalypse.'
+      },
+      {name: 'Mischevious Ice Cream Promenade',
+        description: 'Tour around to all your local ice cream & gelato favorites. Get a taster spoon of all the good-looking flavors. For extra weirdness points, blindfold one person. To avoid burning out too early, only sample flavors at each place- don\'t actually buy any ice cream. Tip super generously. Giggle tremendously while running out so that all other customers assume you two hooligans are surely up to no good.'
+
+      }],
+  8: [{name: 'Roadtrip Game- City Walking Edition',
+        description: 'A new twist on the classic roadtrip game of looking out the window and seeing whose side has more (cows/ice cream stands/bubble gum factories). At each intersection, alternate who chooses which way to turn. After their choice, count up however many (flowers/babies screaming/used needles/couples making out surreptitiously) you pass before the next intersection.'
+      }],
+  9: [{name: 'Stair Climber',
+        description: 'Start in a hilly part of town, with back pathways criss-crossing their way through alleys and community gardens to the top of a hill. Climb said stairs. Then luge/walk/bike/scoot/Uber/dragon/bobsled to the next hill, and repeat!'
+      }],
+  10: []
+};
