64384fca352d4f7d3960f0eda70c32681034d1aa
ClimbsRocks <climbsbytes@gmail.com>
2016-02-21 20:11:07 -0800

    rough ideas i haven't typed up a description for

---
+++ dateIdeasDraft.js
@@ -0,0 +1,45 @@
+
+/*
+format: 
+energyLevel: name
+description
+// separate on newline characters first. then every even one separate on the colon. 
+
+7: Make up our own games together
+
+play music for each other from your music collection. maybe while driving a car. 
+play nothing but music covers for each other for hours at a time. 
+build a pillow fort!
+pick one of each others' favorite dishes, and see if you can create something inspired by that, but different enough to not be compared to it. 
+grab 100 stickers each. hide them throughout each others stuff. whoever finds one last wins. 
+go enjoy some of the enormous previously-industrial spaces in the city. this doubles as hanging out with fun burning man camps :)
+bike or drive to one of the empty bluffs overlooking the ocean. hammock or blanket under the stars while the waves wash in from the West. 
+pizza tasting night- go to a ton of pizza places. buy only one slice at each place (not one slice each- one slice). 
+have a night where you only wear things from the other persons' wardrobe. 
+go whale watching to the Farralon Islands!
+take a walking tour of different neighborhoods. there are apps for this, or do it with a guide. 
+make christmas cookies together in the middle of the year!
+try acro-yoga together. the person with the most bruises wins (they trusted their partner the most!)
+try all the slides around. even the ones that don't exist. 
+run down Lombard street. then run up it! try doing this on something wheeled where the other person has all the control (shopping cart, tandem bike, etc.)
+go through chinatown. 
+the russian spas!
+see how many stickers or notes or googly eyes you can hide in the other persons' stuff by the end of the night. 
+formal picnic in the park. ideally in thrift store clothes. 
+make your own candles!
+try your hand at pottery
+anything at that intercultural transcendental center phillip ventura went to. buddhist chanting, cathartic meditation. it's something like the iscc. 
+ride a train all night. 
+spell out something with your GPS tracks
+make all decisions with the flip of a coin (maybe just the navigational decisions?)
+go through an entire show together that neither of you has watched, but is a classic (The Godfather?). read all the synopses in one night. 
+walk around one of the local university campuses.
+fly a kite! 
+
+
+Relatively fertile and unexplroed territory:
+bike rides
+Scoot
+making up our own games
+
+*/
